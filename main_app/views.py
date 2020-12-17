4b8f91cf21ce2e53fad03c791cc02563ab43b06d
Michael Grebowicz <maciejgrebowicz@gmail.com>
2020-12-17 13:57:37 -0800

    create Dog model, migrate, implement detail page

--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-class Dog:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Tex','golden retreiver', 'super active', 3),
-  Dog('Fido', 'dachsund', 'very long', 0),
-  Dog('Rover', 'chihuahua', '3 legged dog', 4)
-]
-
+from .models import Dog
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
 def about(request):
   return render(request, 'about.html')
 def dogs_index(request):
+  dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', { 
     'dogs': dogs 
   })
+def dogs_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', {
+    'dog': dog
+    })
