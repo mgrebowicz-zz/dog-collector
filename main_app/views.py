b8eadc01d4e83a23a1cb5605794fb3b417110e5e
Michael Grebowicz <maciejgrebowicz@gmail.com>
2020-12-28 21:48:56 -0800

    Add/implement Feeding model

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .models import Dog
+from .forms import FeedingForm
 
 # Create your views here.
+class DogCreate(CreateView):
+  model = Dog
+  fields = '__all__'
+  success_url = '/dogs/'
+
+class DogUpdate(UpdateView):
+  model = Dog
+  fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
+
 def home(request):
   return render(request, 'home.html')
+
 def about(request):
   return render(request, 'about.html')
+
 def dogs_index(request):
   dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', { 
     'dogs': dogs 
   })
+
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
+  # toys_dog_doesnt_have = Toy.objects.exclude(id__in=dog.toys.all().values_list('id'))
+  feeding_form = FeedingForm()
   return render(request, 'dogs/detail.html', {
-    'dog': dog
-    })
+    'dog': dog, 'feeding_form': feeding_form,
+    # 'toys': toys_dog_doesnt_have
+  })
+
+def add_feeding(request, dog_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
